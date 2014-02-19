d9f46651bb8428b3b6d9f2fc71d343e85656283a
Serzh Nechyporchuk <nechyporchuk@gmail.com>
2014-02-19 17:58:15 +0200

    fix demo styles

--- demo/scripts/app.js
+++ demo/scripts/app.js
@@ -1,12 +1,12 @@
 //TODO: current solution doesn't support direct access to routes other than /index
 angular.module('angular-w-demo', ['angular-w', 'ngRoute', 'ngSanitize'], function($routeProvider, $locationProvider) {
   $routeProvider.when('/', {
+    templateUrl: 'demo/formstamp.html'
+  });
+  $routeProvider.when('/widgets', {
     templateUrl: 'demo/widgets.html'
   });
-  $routeProvider.when('/index.html', {
-    templateUrl: 'demo/widgets.html'
-  });
-  $routeProvider.when('/formbuilder.html', {
+  $routeProvider.when('/formbuilder', {
     templateUrl: 'demo/formbuilder.html'
   });
   $locationProvider.html5Mode(true);
