a105b491fb5794819f2f86c96cc588871f72fe8a
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2014-02-19 19:13:49 +0400

    fix & cleanup tags input

--- build/angular-w.js
+++ build/angular-w.js
@@ -1475,7 +1475,8 @@ angular.module('angular-w', []).run(['$templateCache', function($templateCache) 
         scope: {
           invalid: '=',
           items: '=',
-          limit: '='
+          limit: '=',
+          disabled: '@'
         },
         require: '?ngModel',
         replace: true,
@@ -1494,39 +1495,46 @@ angular.module('angular-w', []).run(['$templateCache', function($templateCache) 
           $scope.getItemLabel = function(item) {
             return item;
           };
-          $scope.selection = function(item) {
+          $scope.getItemValue = function(item) {
+            return item;
+          };
+          $scope.selectItem = function(item) {
             if ((item != null) && item.length > 0 && indexOf($scope.selectedItems, item) === -1) {
               $scope.selectedItems.push(item);
-              $scope.search = '';
+              return $scope.search = '';
             }
-            return $scope.hideDropDown();
           };
-          $scope.deselect = function(item) {
+          $scope.unselectItem = function(item) {
             var index;
             index = indexOf($scope.selectedItems, item);
             if (index > -1) {
               return $scope.selectedItems.splice(index, 1);
             }
           };
-          $scope.reset = function() {
-            $scope.selectedItems = [];
-            $scope.focus = true;
-            return search('');
+          $scope.onEnter = function(event) {
+            $scope.selectItem($scope.activeItem || $scope.search);
+            return true;
           };
-          $scope.activeKeys = function(event) {
-            if (event.keyCode === 13) {
-              return $scope.active = true;
-            }
+          $scope.onPgup = function(event) {
+            $scope.move(-11);
+            return true;
+          };
+          $scope.onPgdown = function(event) {
+            $scope.move(11);
+            return true;
+          };
+          $scope.move = function(d) {
+            var activeIndex, items;
+            items = $scope.shownItems;
+            activeIndex = (indexOf($scope.shownItems, $scope.activeItem) || 0) + d;
+            activeIndex = Math.min(Math.max(activeIndex, 0), items.length - 1);
+            return $scope.activeItem = items[activeIndex];
           };
           $scope.$watch('search', search);
-          $scope.hideDropDown = function() {
-            return $scope.active = false;
-          };
-          $scope.selectedItems = [];
-          return search('');
+          return $scope.selectedItems = [];
         },
         link: function(scope, element, attrs, ngModelCtrl, transcludeFn) {
-          var getActiveIndex, scroll, setViewValue;
+          var scroll, setViewValue;
           if (ngModelCtrl) {
             setViewValue = function(newValue, oldValue) {
               if (!angular.equals(newValue, oldValue)) {
@@ -1539,60 +1547,6 @@ angular.module('angular-w', []).run(['$templateCache', function($templateCache) 
             };
             addValidations(attrs, ngModelCtrl);
           }
-          attrs.$observe('disabled', function(value) {
-            return scope.disabled = value;
-          });
-          scope.$watch('selectedItems', function() {
-            var childScope;
-            childScope = scope.$new();
-            childScope.items = scope.selectedItems;
-            return transcludeFn(childScope, function(clone) {
-              var link;
-              if (clone.text().trim() !== "") {
-                link = element[0].querySelector('a.w-multi-select-active');
-                return angular.element(link).empty().append(clone);
-              }
-            });
-          });
-          $window.addEventListener('click', function(e) {
-            var parent;
-            parent = $(e.target).parents('div.w-multi-select')[0];
-            if (parent !== element[0]) {
-              return scope.$apply(function() {
-                scope.hideDropDown();
-                return scope.selection(scope.search);
-              });
-            }
-          });
-          scope.onEnter = function(event) {
-            scope.selection(scope.activeItem || scope.search);
-            scope.focus = true;
-            return event.preventDefault();
-          };
-          scope.onPgup = function(event) {
-            scope.move(-11);
-            return event.preventDefault();
-          };
-          scope.onPgdown = function(event) {
-            scope.move(11);
-            return event.preventDefault();
-          };
-          scope.onTab = function() {};
-          scope.onEsc = function() {
-            scope.hideDropDown();
-            return scope.focus = true;
-          };
-          getActiveIndex = function() {
-            return indexOf(scope.shownItems, scope.activeItem) || 0;
-          };
-          scope.move = function(d) {
-            var activeIndex, items;
-            items = scope.shownItems;
-            activeIndex = getActiveIndex() + d;
-            activeIndex = Math.min(Math.max(activeIndex, 0), items.length - 1);
-            scope.activeItem = items[activeIndex];
-            return scroll();
-          };
           return scroll = function() {
             var delayedScrollFn;
             delayedScrollFn = function() {
--- src/tags.coffee
+++ src/tags.coffee
@@ -6,12 +6,12 @@ angular
       invalid: '='
       items: '='
       limit: '='
+      disabled: '@'
     require: '?ngModel'
     replace: true
     transclude: true
     templateUrl: "/templates/multi-select.html"
     controller: ($scope, $element, $attrs) ->
-
       search = (q) ->
         $scope.shownItems = filter(q, $scope.items).slice(0, $scope.limit)
         if $scope.shownItems.length == 0
@@ -22,37 +22,43 @@ angular
       $scope.getItemLabel = (item)->
         item
 
+      $scope.getItemValue = (item)->
+        item
+
       #TODO: why is this method's name a noun instead of a verb?
-      $scope.selection = (item)->
+      $scope.selectItem = (item)->
         if item? and item.length > 0 and indexOf($scope.selectedItems, item) == -1
           $scope.selectedItems.push(item)
           $scope.search = ''
-        $scope.hideDropDown()
 
-
-      $scope.deselect = (item)->
+      $scope.unselectItem = (item)->
         index = indexOf($scope.selectedItems, item)
         if index > -1
           $scope.selectedItems.splice(index, 1)
 
-      $scope.reset = ->
-        $scope.selectedItems = []
-        $scope.focus = true
-        search('')
+      $scope.onEnter = (event) ->
+        $scope.selectItem($scope.activeItem || $scope.search)
+        true
 
-      $scope.activeKeys = (event) ->
-        if event.keyCode == 13
-          $scope.active = true
+      $scope.onPgup = (event) ->
+        $scope.move(-11)
+        true
+
+      $scope.onPgdown = (event) ->
+        $scope.move(11)
+        true
+
+      $scope.move = (d) ->
+        items = $scope.shownItems
+        activeIndex = (indexOf($scope.shownItems, $scope.activeItem) || 0) + d
+        activeIndex = Math.min(Math.max(activeIndex,0), items.length - 1)
+        $scope.activeItem = items[activeIndex]
 
 
       $scope.$watch 'search', search
 
-      $scope.hideDropDown = ->
-        $scope.active = false
-
       # run
       $scope.selectedItems = []
-      search('')
 
     link: (scope, element, attrs, ngModelCtrl, transcludeFn) ->
       if ngModelCtrl
@@ -67,54 +73,6 @@ angular
 
         addValidations(attrs, ngModelCtrl)
 
-      attrs.$observe 'disabled', (value) ->
-        scope.disabled = value
-
-      scope.$watch  'selectedItems', ->
-        childScope = scope.$new()
-        childScope.items = scope.selectedItems
-        transcludeFn childScope, (clone) ->
-          if clone.text().trim() isnt ""
-            link = element[0].querySelector('a.w-multi-select-active')
-            angular.element(link).empty().append(clone)
-
-      # Hide drop down list on click elsewhere
-      $window.addEventListener 'click', (e) ->
-        parent = $(e.target).parents('div.w-multi-select')[0]
-        if parent != element[0]
-          scope.$apply ->
-            scope.hideDropDown()
-            scope.selection(scope.search)
-
-      scope.onEnter = (event) ->
-        scope.selection(scope.activeItem || scope.search)
-        scope.focus=true
-        event.preventDefault()
-
-      scope.onPgup = (event) ->
-        scope.move(-11)
-        event.preventDefault()
-
-      scope.onPgdown = (event) ->
-        scope.move(11)
-        event.preventDefault()
-
-      scope.onTab = ->
-        # scope.selection(scope.search || scope.activeItem)
-
-      scope.onEsc = ->
-        scope.hideDropDown()
-        scope.focus=true
-
-      getActiveIndex = ->
-        indexOf(scope.shownItems, scope.activeItem) || 0
-
-      scope.move = (d) ->
-        items = scope.shownItems
-        activeIndex = getActiveIndex() + d
-        activeIndex = Math.min(Math.max(activeIndex,0), items.length - 1)
-        scope.activeItem = items[activeIndex]
-        scroll()
 
       scroll = ->
         delayedScrollFn = ->
