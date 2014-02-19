fdffc6d9eb3e8922e34025faee4a63ec048f3aa5
dmitry-n <nechayevdv@gmail.com>
2014-02-19 16:38:17 +0400

    add formbuilder to angular routes

--- demo/scripts/app.js
+++ demo/scripts/app.js
@@ -1,9 +1,13 @@
+//TODO: current solution doesn't support direct access to routes other than /index
 angular.module('angular-w-demo', ['angular-w', 'ngRoute', 'ngSanitize'], function($routeProvider, $locationProvider) {
   $routeProvider.when('/', {
     templateUrl: 'demo/widgets.html'
   });
   $routeProvider.when('/index.html', {
     templateUrl: 'demo/widgets.html'
+  });
+  $routeProvider.when('/formbuilder.html', {
+    templateUrl: 'demo/formbuilder.html'
   });
   $locationProvider.html5Mode(true);
 });
