abd59f98b201b3faec180fbb5783724011de8ed7
Serzh Nechyporchuk <nechyporchuk@gmail.com>
2014-02-21 15:49:58 +0200

    restructure demo

--- demo/scripts/app.js
+++ demo/scripts/app.js
@@ -1,14 +1,20 @@
 //TODO: current solution doesn't support direct access to routes other than /index
+window.widgets = ['Select', 'MultiSelect', 'Combo', 'Tags', 'Radio', 'Checkbox']
+
 angular.module('angular-w-demo', ['angular-w', 'ngRoute', 'ngSanitize'], function($routeProvider, $locationProvider) {
   $routeProvider.when('/', {
     templateUrl: 'demo/formstamp.html',
     controller: 'RootExampleController'
   });
-  $routeProvider.when('/widgets', {
-    templateUrl: 'demo/widgets.html'
+  angular.forEach(window.widgets, function(widget) {
+    $routeProvider.when('/widgets/' + widget.toLowerCase(), {
+      templateUrl: '/demo/widgets/' + widget.toLowerCase() + '.html'
+    });
   });
   $routeProvider.when('/formbuilder', {
     templateUrl: 'demo/formbuilder.html'
   });
   $locationProvider.html5Mode(true);
+}).run( function($rootScope) {
+  $rootScope.widgets = window.widgets;
 });
