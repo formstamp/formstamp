88e1e1e68be4c1823b733764d785832b28b8efe7
niquola <niquola@gmail.com>
2014-02-26 17:03:15 +0400

    time input

--- build/formstamp.js
+++ build/formstamp.js
@@ -1194,7 +1194,6 @@ angular.module('formstamp', []).run(['$templateCache', function($templateCache) 
       return {
         restrict: "A",
         scope: {
-          items: '=',
           disabled: '=ngDisabled',
           freetext: '@',
           "class": '@'
@@ -1202,18 +1201,59 @@ angular.module('formstamp', []).run(['$templateCache', function($templateCache) 
         require: '?ngModel',
         replace: true,
         template: function(el) {
-          return "<input type=\"text\"/>";
+          var h, hours, m, minutes, num, res, timeoptions, zh, _i, _j, _len, _len1;
+          hours = (function() {
+            var _i, _results;
+            _results = [];
+            for (num = _i = 0; _i <= 23; num = ++_i) {
+              _results.push(num);
+            }
+            return _results;
+          })();
+          minutes = ['00', '15', '30', '45'];
+          res = [];
+          for (_i = 0, _len = hours.length; _i < _len; _i++) {
+            h = hours[_i];
+            zh = h < 10 ? "0" + h : h;
+            for (_j = 0, _len1 = minutes.length; _j < _len1; _j++) {
+              m = minutes[_j];
+              res.push("<option value='" + zh + ":" + m + "'/>");
+            }
+          }
+          timeoptions = res.join('');
+          return "<div class=\"fs-time\">\n  <input\n    ng-model=\"value\"\n    class=\"form-control\"\n    ng-disabled=\"disabled\"\n    list=\"time\"\n    type=\"text\"/>\n  <span class=\"glyphicon glyphicon-time\" ></span>\n  <datalist id=\"time\">\n  " + timeoptions + "\n  </datalist>\n</div>";
         },
-        controller: function($scope, $element, $attrs, $filter, $timeout) {},
+        controller: function($scope, $element, $attrs, $filter, $timeout) {
+          var patterns;
+          patterns = [/^[012]/, /^([0-1][0-9]|2[0-3]):?/, /^([0-1][0-9]|2[0-3]):?[0-5]/, /^([0-1][0-9]|2[0-3]):?([0-5][0-9])/];
+          return $scope.$watch('value', function(ev) {
+            var p, res, value, _i, _len;
+            if ($scope.value == null) {
+              return;
+            }
+            value = '';
+            for (_i = 0, _len = patterns.length; _i < _len; _i++) {
+              p = patterns[_i];
+              res = $scope.value.match(p);
+              if (res) {
+                value = res[0];
+              }
+            }
+            if (value.length > 2) {
+              value = value.replace(/^(\d\d)([^:]*)$/, "$1:$2");
+            }
+            return $scope.value = value;
+          });
+        },
         link: function(scope, element, attrs, ngModelCtrl, transcludeFn) {
           if (ngModelCtrl) {
-            scope.$watch('item', function(newValue, oldValue) {
+            scope.$watch('value', function(newValue, oldValue) {
               if (newValue !== oldValue) {
-                return ngModelCtrl.$setViewValue(scope.item);
+                return ngModelCtrl.$setViewValue(scope.value);
               }
             });
             return ngModelCtrl.$render = function() {
-              return scope.item = ngModelCtrl.$viewValue;
+              return scope.value = ngModelCtrl.$viewValue;
             };
           }
         }
--- src/time.coffee
+++ src/time.coffee
@@ -3,25 +3,60 @@ angular
 .directive "fsTime", ['$compile', ($compile) ->
   restrict: "A"
   scope:
-    items: '='
     disabled: '=ngDisabled'
     freetext: '@'
     class: '@'
   require: '?ngModel'
   replace: true
   template: (el)->
+    hours = (num for num in [0..23])
+    minutes = ['00','15','30','45']
+    res = []
+    for h in hours
+      zh = if h < 10 then "0#{h}" else h
+      for m in minutes
+        res.push "<option value='#{zh}:#{m}'/>"
+    timeoptions = res.join('')
+
     """
-    <input type="text"/>
+    <div class="fs-time">
+      <input
+        ng-model="value"
+        class="form-control"
+        ng-disabled="disabled"
+        list="time"
+        type="text"/>
+      <span class="glyphicon glyphicon-time" ></span>
+      <datalist id="time">
+      #{timeoptions}
+      </datalist>
+    </div>
     """
 
   controller: ($scope, $element, $attrs, $filter, $timeout) ->
+    patterns = [
+      /^[012]/
+      /^([0-1][0-9]|2[0-3]):?/
+      /^([0-1][0-9]|2[0-3]):?[0-5]/
+      /^([0-1][0-9]|2[0-3]):?([0-5][0-9])/
+    ]
+
+    $scope.$watch 'value',(ev)->
+      return unless $scope.value?
+      value = ''
+      for p in patterns
+        res = $scope.value.match(p)
+        if res
+          value = res[0]
+      value = value.replace(/^(\d\d)([^:]*)$/,"$1:$2") if value.length > 2
+      $scope.value = value
 
   link: (scope, element, attrs, ngModelCtrl, transcludeFn) ->
     if ngModelCtrl
-      scope.$watch 'item', (newValue, oldValue) ->
+      scope.$watch 'value', (newValue, oldValue) ->
         if newValue isnt oldValue
-          ngModelCtrl.$setViewValue(scope.item)
+          ngModelCtrl.$setViewValue(scope.value)
 
       ngModelCtrl.$render = ->
-        scope.item = ngModelCtrl.$viewValue
+        scope.value = ngModelCtrl.$viewValue
 ]
