db232a25ba67f252b83e476dac99e5dfce2ca8bd
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2014-03-04 16:28:43 +0400

    finished demo deploy script

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -6,8 +6,17 @@ set +e # break script on first error
 
 # make sure script is run from correct place
 if [ ! -d .git ]; then
-    echo "You should invoke this script from ROOT of formstamp GIT repo"
-    echo "demo/deploy.sh"
+    echo "ERROR: You should run this script from ROOT of formstamp GIT repo"
+    echo "Example: demo/deploy.sh"
+    exit 1
+fi
+
+# make sure formstamp.github.io repo exist
+if [ ! -d ../formstamp.github.io/.git ]; then
+    CWD=`pwd`
+    echo "ERROR: You should have formstamp.github.io repo as a sibling directory of formstamp main repo. Run following command:"
+    echo "cd `dirname $CWD` && git clone git@github.com:formstamp/formstamp.github.io.git"
+    exit 1
 fi
 
 source ~/.nvm/nvm.sh # make sure nvm command is available
