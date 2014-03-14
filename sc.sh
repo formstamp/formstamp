0613a566c9f540022b5c1a60b78de4d1fbf5dac7
Serzh Nechyporchuk <nechyporchuk@gmail.com>
2014-03-14 21:03:05 +0200

    sauce travis

---
+++ sc.sh
@@ -0,0 +1,10 @@
+TARBALL_NAME=sc.tar.gz
+READY_FILE="connect-ready-$RANDOM"
+wget https://saucelabs.com/downloads/sc-4.0-latest-linux.tar.gz -O $TARBALL_NAME
+tar xvf $TARBALL_NAME
+cd sc-4.0-linux
+bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -f $READY_FILE > /dev/null &
+
+while [ ! -f $READY_FILE ]; do
+    sleep .5
+done
