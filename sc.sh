eac5b2ad31780d660b85d399fc13995102b09ea0
Serzh Nechyporchuk <nechyporchuk@gmail.com>
2014-03-16 00:01:37 +0200

    add id for sl for parallel tests

--- sc.sh
+++ sc.sh
@@ -3,7 +3,7 @@ READY_FILE="connect-ready-$RANDOM"
 wget https://saucelabs.com/downloads/sc-4.0-latest-linux.tar.gz -O $TARBALL_NAME
 tar xvf $TARBALL_NAME
 cd sc-4.0-linux
-bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -f $READY_FILE > /dev/null &
+bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -f $READY_FILE -i $TRAVIS_JOB_NUMBER > /dev/null &
 
 while [ ! -f $READY_FILE ]; do
     sleep .5
